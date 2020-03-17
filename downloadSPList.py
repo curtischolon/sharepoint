15aa5567e8b0142cd5a9ed7eb1e32b52b417ba7d
Curtis Cholon <u80lbYFay7Su>
2020-03-17 15:08:23 -0700

    added dataframe to excel

--- downloadSPList.py
+++ downloadSPList.py
@@ -1,11 +1,66 @@
 from shareplum import Site
 from requests_ntlm import HttpNtlmAuth
+from credentials import Credentials
+import pandas as pd
 
-cred = HttpNtlmAuth('ccholon', '3.Ch0l0n.k1ds')
+
+creds = Credentials()
+
+cred = HttpNtlmAuth(creds.username, creds.password)
 site = Site('http://tng/SharedServices/AR', auth=cred)
 
-list = site.List('Document_Circulation').get_list_items()
-for item in list:
+rows = site.List('Document_Circulation').get_list_items()
+
+columns = [
+    'Service_Location',
+    'Dealer_Name',
+    'Master',
+    'Chain_Master',
+    'Dealer_Number',
+    'Statements',
+    'Invoices',
+    'Credits',
+    'Fax Number',
+    'Attn To:',
+    'Output',
+    'Created',
+    'Modified',
+    'Item Type',
+    'Path',
+    'Email Address'
+]
+
+dealer_list = []
+keys = []
+for row in rows:
+    keys = keys + list(row.keys())
+    # keys = list(row.keys())
+    # r = [row[i] for i in keys]
+    r = [
+        row.get('Service_Location'),
+        row.get('Dealer_Name'),
+        row.get('Master'),
+        row.get('Chain_Master'),
+        row.get('Dealer_Number'),
+        row.get('Statements'),
+        row.get('Invoices'),
+        row.get('Credits'),
+        row.get('Fax Number', ''),
+        row.get('Attn To:', ''),
+        row.get('Output', ''),
+        row.get('Created'),
+        row.get('Modified'),
+        row.get('Item Type'),
+        row.get('Path', ''),
+        row.get('Email Address', '')
+    ]
+    dealer_list.append(r)
+
+keys = set(keys)
+print(keys)
+df = pd.DataFrame(dealer_list, columns=columns)
+df.to_excel('Customer_Documents.xlsx', index=False, header=True)
+
     
 
 
